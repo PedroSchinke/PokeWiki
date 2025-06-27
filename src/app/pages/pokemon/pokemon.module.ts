cf488980887f2904b88a51f7735baffbfd50db9c
Pedro Schinke <pedro.schinke@outlook.com>
2025-06-26 22:20:42 -0300

    adicionando imagens e novas informacoes do pokemon no card

--- src/app/pages/pokemon/pokemon.module.ts
+++ src/app/pages/pokemon/pokemon.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { PokemonPageRoutingModule } from './pokemon-routing.module';
 import { PokemonPage } from './pokemon.page';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatChip} from "@angular/material/chips";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatFabButton} from "@angular/material/button";
 
 @NgModule({
   imports: [
@@ -11,6 +22,17 @@ import { PokemonPage } from './pokemon.page';
     FormsModule,
     IonicModule,
     PokemonPageRoutingModule,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatChip,
+    MatCardImage,
+    MatCardTitle,
+    MatCardSubtitle,
+    NgOptimizedImage,
+    MatIcon,
+    MatFabButton,
+    MatIconModule
   ],
   declarations: [PokemonPage]
 })
--- src/app/pages/pokemon/pokemon.page.ts
+++ src/app/pages/pokemon/pokemon.page.ts
@@ -9,6 +9,7 @@ interface Pokemon {
     name: string;
   };
   types: Type[];
+  sprites: {};
 }
 
 interface Type {
@@ -26,13 +27,18 @@ interface Type {
 export class PokemonPage implements OnInit {
   private id: string | null = null;
   private pokeApiUrl = environment.pokeApiURL;
-  protected pokemon:Pokemon = {
+  protected pokemon:any = {
     name: '',
     species: {
       name: ''
     },
-    types: []
+    types: [],
+    sprites: {}
   };
+  protected pokemonImages:any = [];
+  protected pokemonImage = null;
+  protected totalImages = 0;
+  protected imageIndex = 0;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -47,7 +53,69 @@ export class PokemonPage implements OnInit {
       .subscribe((data: any) => {
         console.log(data);
         this.pokemon = data;
+
+        this.pokemon.abilities.forEach((ability:any) => {
+          this.getAbilities(ability.ability.url);
+        });
+
+        Object.values(this.pokemon.sprites.other['official-artwork']).forEach((img) => {
+          if (img && typeof img === 'string') this.pokemonImages.push(img);
+        });
+
+        Object.values(this.pokemon.sprites).forEach((img) => {
+          if (img && typeof img === 'string') this.pokemonImages.push(img);
+        });
+
+        this.totalImages = this.pokemonImages.length;
+
+        if (this.pokemonImages.length > 0) {
+          this.pokemonImage = this.pokemonImages[0];
+        }
+
+        console.log(this.pokemonImages)
       });
+  }
+
+  primeiraLetraMaiuscula(string:string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  getAbilities(url:string) {
+    // this.http.get(url.slice(0, -1))
+    //   .subscribe((data:any) => {
+    //     console.log(data);
+    //   });
+  }
+
+  previousImage() {
+    const previousImage = this.pokemonImages[this.imageIndex - 1];
+
+    if (!previousImage) {
+      const lastIndex = this.pokemonImages.length - 1;
+      this.pokemonImage = this.pokemonImages[lastIndex];
+      this.imageIndex = lastIndex;
+
+      return;
+    }
+
+    const previousIndex = this.imageIndex - 1;
+    this.pokemonImage = this.pokemonImages[previousIndex];
+    this.imageIndex = previousIndex
+  }
+
+  nextImage() {
+    const nextImage = this.pokemonImages[this.imageIndex + 1];
+
+    if (!nextImage) {
+      this.pokemonImage = this.pokemonImages[0];
+      this.imageIndex = 0;
+
+      return;
+    }
+
+    const nextIndex = this.imageIndex + 1;
+    this.pokemonImage = this.pokemonImages[this.imageIndex + 1];
+    this.imageIndex = nextIndex;
   }
 }
 
