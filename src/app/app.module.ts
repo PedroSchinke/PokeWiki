158befc7aafdbce7b76cc45c6a13311053c342be
Pedro Schinke <pedro.schinke@outlook.com>
2025-07-03 22:27:18 -0300

    criado header da aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +14,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatCardModule, MatCardTitle } from "@angular/material/card";
+import { MatIcon } from "@angular/material/icon";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,7 +28,11 @@ import { MatCardModule, MatCardTitle } from "@angular/material/card";
     MatInputModule,
     MatFormFieldModule,
     MatCardModule,
-    MatCardTitle
+    MatCardTitle,
+    MatIcon,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem
   ],
   providers: [
     {
