5af5e8c2d1ac83d51d33428ec72e817d3bf3fee8
Pedro Schinke <pedro.schinke@outlook.com>
2025-07-05 22:59:40 -0300

    adicionados pokemons favoritos na tela de dados do usuario

--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -3,13 +3,16 @@ import { IonicModule } from "@ionic/angular";
 import {
   MatCard,
   MatCardContent,
-  MatCardHeader,
+  MatCardHeader, MatCardImage,
   MatCardTitle
 } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
-import { NgIf } from "@angular/common";
-import primeiraLetraMaiuscula from "../../helpers/primeiraLetraMaiuscula";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import { AuthService } from "../../services/auth.service";
+import primeiraLetraMaiuscula from "../../helpers/primeiraLetraMaiuscula";
+import { HttpClient } from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -23,21 +26,39 @@ import { AuthService } from "../../services/auth.service";
     MatCardTitle,
     MatIcon,
     NgIf,
+    NgForOf,
+    MatCardImage,
+    NgOptimizedImage,
+    RouterLink,
   ]
 })
 export class UserPage implements OnInit {
+  private pokeApiUrl = environment.pokeApiURL;
   protected nome = '';
   protected email = '';
-  protected favoritos = [];
+  protected favoritosIds = [];
+  protected pokemonsFavoritos: any = [];
 
-  constructor(private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
     const user = this.authService.getUser();
 
     this.nome = user.name;
     this.email = user.email;
-    this.favoritos = user.favorites;
+
+    this.favoritosIds = this.authService.getFavoritePokemonIds();
+
+    this.getPokemonsFavoritos();
+  }
+
+  getPokemonsFavoritos() {
+    this.favoritosIds.forEach((id) => {
+      this.http.get(`${this.pokeApiUrl}/pokemon/${id}`)
+        .subscribe((response) => {
+          this.pokemonsFavoritos.push(response);
+        });
+    });
   }
 
   protected readonly primeiraLetraMaiuscula = primeiraLetraMaiuscula;
